b94808a81a84c9a972ab0f0e15362ffeceeb0caa
dmontigny <dmontigny@yahoo.com>
2018-09-20 19:38:59 -0400

    unknown commit- no notes

--- bookstore/clsbookback.py
+++ bookstore/clsbookback.py
@@ -8,29 +8,6 @@ class dbbooks:
         self.cur = self.conn.cursor()
 
 #        db = 'pyBase'
-        TABLES = {}
-
-        TABLES['titles'] = (
-            "CREATE TABLE `books` ("
-            "  `idtitle` INT(11) NOT NULL AUTO_INCREMENT,"
-            "  `title` VARCHAR(75),"
-            "  `author` VARCHAR(25) NOT NULL,"
-            "  `year` INT(4),"
-            "  `ISBN` BIGINT(13),"
-            "  PRIMARY KEY (`idtitle`)"
-            ") ENGINE=InnoDB")
-
-        for name, ddl in TABLES.items():
-            try:
-                print("Creating table {}: ".format(name), end='')
-                self.cur.execute(ddl)
-            except mysql.connector.Error as err:
-                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                    print("already exists.")
-                else:
-                    print(err.msg)
-            else:
-                print("OK")
 
     def create_conn(self):
         from socket import gethostname
