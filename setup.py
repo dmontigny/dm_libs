862362087275c410a5880525143f5b95a8b12352
dmontigny <dmontigny27@gmail.com>
2020-01-22 14:06:42 -0500

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dm_libs',
+    version='1.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='dmontigny',
+    author_email='dmontigny27@gmail.com',
+    description=''
+)
