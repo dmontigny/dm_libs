081ec184cef561b42b997fbd250327dd34635b4a
dmontigny <dmontigny27@gmail.com>
2020-03-05 19:05:55 -0500

    added a get tree function to grab the entire tree folders and files

--- cls_files.py
+++ cls_files.py
@@ -28,11 +28,33 @@ class ClassBrowser(object):
         if self.log:
             mylogger.get_Logger('cls_files')
         self.location = None
+        self.tree = {}
         self.folders = {}
         self.files = {}
         self.target = None
         if self.log:
             mylogger.info(f'file searcher created')
+
+    def get_tree(self, location="S:\OBC"):
+        self.location = path.normpath(location)
+        print(f"get files: {location}")
+        if self.location[0] == "\\" and self.location[1] != "\\":
+            self.location = "\\" + self.location
+
+        # TODO: cannot read network share folders
+        if self.log:
+            mylogger.info(f'getting files at {self.location}')
+        self.files = {}
+        for root, dirs, files in walk(self.location):
+            for loc in dirs:
+                self.tree[loc] = path.join(root, loc)
+            for file in files:
+                self.tree[file] = path.join(root, file)
+                # self.tree[file.split(("\\", "/")[op_sys == "Windows"])[-1]] = path.join(root, file)
+
+        if self.log:
+            mylogger.info(f'found {len(self.files)} folders at {self.location}')
+        return self.tree
 
     def get_dirs(self, location="S:\OBC"):
         self.location = path.normpath(location)
